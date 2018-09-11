2b1a5ca8a6161dbddc61496e80b70ad1914fdd01
VICTOR ULISES RAMAYO AKE <110300047@ucaribe.edu.mx>
2018-09-11 12:13:55 -0500

    Creacion de FormActivity

---
+++ app/src/main/java/comestible/oblea/com/obleacomestibleapp/Activity/FormActivity.java
@@ -0,0 +1,15 @@
+package comestible.oblea.com.obleacomestibleapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comestible.oblea.com.obleacomestibleapp.R;
+
+public class FormActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}

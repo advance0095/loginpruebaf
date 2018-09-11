c4099c6568daecb7c2704da97dc03b5d4b884bf4
VICTOR ULISES RAMAYO AKE <110300047@ucaribe.edu.mx>
2018-09-11 12:05:03 -0500

    Estructura Modificada

---
+++ app/src/main/java/comestible/oblea/com/obleacomestibleapp/Activity/BaseActivity.java
@@ -0,0 +1,32 @@
+package comestible.oblea.com.obleacomestibleapp.Activity;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+/*
+    Created By Victor Ramayo
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    public SweetAlertDialog mSweetAlertDialog;
+
+    protected void showBackButton() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    protected void showHomeButton() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+    }
+
+    public void setActionbarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+}
